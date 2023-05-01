421e8119e90662fda2d61bff48e95e3b48631228
Maxim Kolotovkin Cactus Soft <maxim.kolotovkin@cactussoft.biz>
2023-05-01 17:39:41 +0400

    sass

---
+++ README.md
@@ -0,0 +1,15 @@
+# Test CSS Project
+
+Install sass globally
+
+```
+npm install -g sass 
+```
+
+Build styles
+
+```
+sass ./sass/a.scss ./css/a.css 
+sass ./sass/b.scss ./css/b.css 
+```
+
---
+++ index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+
+const express = require("express");
+const app = express();
+const port = 5000;
+app.listen(port);
+console.log("Server on port: " + port);
+
+
+app.get("/page/:pageName", function (request, response) {
+    return response.sendFile(__dirname + "/pages/" + request.params['pageName'] + ".html");
+});
+
+
+app.get("/css/:cssName", function (request, response) {
+    return response.sendFile(__dirname + "/css/" + request.params['cssName']);
+});
+
